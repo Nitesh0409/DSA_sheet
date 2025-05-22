dae47e4ffb0ec784efc9986d1958cd0cdeddd354
Nitesh <niteshkkanojia@gmail.com>
2025-05-22 15:40:43 +0530

    added intuitive approach

--- tree/sheet-ques/LCA.cpp
+++ tree/sheet-ques/LCA.cpp
@@ -1,6 +1,6 @@
 // question link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct TreeNode
@@ -11,16 +11,18 @@ struct TreeNode
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-/*
-go deep as possible till not find p or q
-check in both side left and right if find then
-if one is left other is right return root
-else if one side is not null reture left .
-*/
-
 class Solution
 {
 public:
+
+//recursive approach:
+    /*
+    go deep as possible till not find p or q
+    check in both side left and right if find then
+    if one is left other is right return root
+    else if one side is not null reture left .
+    */
+
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
     {
         if (!root || root == p || root == q)
@@ -33,6 +35,53 @@ public:
             return root;
 
         return left ? left : right;
+    }
+
+
+    //but intituitive approach:
+    /*
+    map parent to every child till we can not find p and q;
+    we store all ancestors of p in set
+    then we just check for ancestor of q if they exist in ancestor of p;
+    */
+
+    TreeNode* LCA(TreeNode* root, TreeNode* p, TreeNode* q){
+
+        //mapping:
+        unordered_map<TreeNode *, TreeNode*> parent;
+        parent[root] = nullptr;
+        stack<TreeNode *> st;
+        st.push(root);
+
+        while(parent.count(p) || parent.count(q)){
+            TreeNode *node = st.top();
+            st.pop();
+
+            if(node->left) {
+                parent[node->left] = node;
+                st.push(node->left);
+            }
+
+            if(node->right){
+                parent[node->right] = node;
+                st.push(node->right);
+            }
+        }
+
+        set<TreeNode *> ancestor;
+
+        // parents of p in set
+        while(p){
+            ancestor.insert(p);
+            p = parent[p];
+        }
+
+        // parent of q is same as p
+        while(!ancestor.count(q)){
+            q = parent[q];
+        }
+
+        return q;
     }
 };
 

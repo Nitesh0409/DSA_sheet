e00aef06802f059da46e21b6376ebb021f76d7ff
Nitesh <niteshkkanojia@gmail.com>
2025-05-26 17:45:39 +0530

    dfs implemented

---
+++ graph/algorithms/dfs.cpp
@@ -0,0 +1,66 @@
+//this algorith is same for undirected and directed graph
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> adjFormation(int v, vector<vector<int>> &edges)
+    {
+        vector<vector<int>> adj(v);
+
+        for (vector<int> &edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u); // for undirected graph
+        }
+
+        return adj;
+    }
+
+    void dfs(vector<vector<int>> &adj, vector<bool> &visited, int u, vector<int> &result)
+    {
+
+        visited[u] = true;
+        result.push_back(u);
+
+        for (int v : adj[u])
+        {
+            if (!visited[v])
+            {
+                dfs(adj, visited, v, result);
+            }
+        }
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    vector<vector<int>> edges = {{0, 1}, {0, 3}, {1, 2}, {3, 2}, {2, 5}};
+    int V = 6;
+
+    // by adj list
+    vector<vector<int>> adj = sol.adjFormation(V, edges);
+
+    vector<bool> visited(V, false);
+    vector<int> result;
+
+    for (int i = 0; i < V; i++){
+        if(!visited[i]){
+            sol.dfs(adj, visited, i, result);
+        }
+    }
+
+        for (int &node : result)
+        {
+            cout << node << " ";
+        }
+
+    return 0;
+}

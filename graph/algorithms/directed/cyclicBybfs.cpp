d0dc60befcdba045cc3fd95990719e69f5076441
Nitesh <niteshkkanojia@gmail.com>
2025-05-27 12:03:10 +0530

    cyclic detection by bfs

--- graph/algorithms/directed/cyclicBybfs.cpp
+++ graph/algorithms/directed/cyclicBybfs.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+//just used kanhs algo if not possible to get all nodes that mean cycle exist 
+
 class Solution
 {
     vector<vector<int>> adjFormation(int v, vector<vector<int>> &edges)
@@ -24,6 +26,41 @@ public:
     bool isCyclic(int V, vector<vector<int>> &edges)
     {
         // code here
+        vector<vector<int>> adj = adjFormation(V, edges);
+
+        //indegree formation
+        vector<int> indegree(V);
+        for (int i = 0; i < V; i++){
+            for(int& v : adj[i]){
+                indegree[v]++;
+            }
+        }
+
+        
+        //kanh's algo
+        queue<int> q;
+        for (int i = 0; i < V; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        int count = 0;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            count++;
+
+            for(int&v : adj[u]){
+                indegree[v]--;
+
+                if(indegree[v] == 0){
+                    q.push(v);
+                }
+            }
+        }
+
+        return (count == V) ? false : true;
     }
 };
 

5fa2ea2e6c013f3798f619fa4e86577ba2584643
Nitesh <niteshkkanojia@gmail.com>
2025-12-19 12:22:14 +0530

    update competitive Programing Folder

---
+++ competitivePrograming/FenwickTree/rangeSum.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class FenwickTree
+{
+private:
+    int n;
+    vector<int> bit;
+
+public:
+    FenwickTree(int size){
+        n = size;
+        bit.assign(n + 1, 0);
+    }
+
+    void update(int idx, int val){
+        while(idx <= n){
+            bit[idx] += val;
+            idx += idx & (-idx); //jump formward
+        }
+    }
+
+    int query (int i){
+        int sum = 0;
+        while( i > 0){
+            sum += bit[i];
+            i -= (i & (-i));  //jump backward
+        }
+
+        return sum;
+    }
+
+    int sumInRange(int l, int r){
+        return query(r) - query(l - 1);
+    }
+
+};
+
+class Solution{
+public:
+    vector<int> SumInRange(vector<int> &nums, vector<pair<int, int>> &queries)
+    {
+        int n = nums.size();
+        FenwickTree ft(n);
+
+        //tree formation
+        for (int i = 1; i <= n; i++){
+            ft.update(i, nums[i-1]);
+        }
+
+        //find sum in range
+        vector<int> result;
+        for (auto &q : queries)
+        {
+            result.push_back(ft.sumInRange(q.first+1, q.second+1));  //making 0- index
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {2, 3, 4, 1, 5, 7, 8, 6};
+    int n = nums.size();
+
+    vector<pair<int, int>> queries = {{1, 4}, {4, 7}, {2, 6}, {0, 7}};
+
+    Solution sol;
+    vector<int> result = sol.SumInRange(nums, queries);
+
+    for (int res: result)
+        cout << res << " ";
+
+        return 0;
+}
--- competitivePrograming/segmentTree/main.cpp
+++ competitivePrograming/segmentTree/main.cpp
@@ -7,10 +7,9 @@ public:
     vector<int> segment;
     int n;
 
+    //------------------------ segment formation O(n) ---------------------------------
     void segmentTreeFormation(vector<int>& nums){
         n = nums.size();
-
-        // int s = (n % 2 == 0) ? 2 * n : 2 * n - 1; //not work here will confirm later
 
         segment.resize(n * 4);
 
@@ -36,25 +35,51 @@ public:
         segment[idx] = segment[left] + segment[right];
     }
 
-    
-    //for getting value for queries
+    //--------------------- node value  O(log n) --------------------------------------
+
     int getNodeValue(int l, int r){
         return query(0, 0, n - 1, l, r);
     }
 
     int query(int idx, int start, int end, int l, int r){
-        
+        //base case
         if(end < l || start > r)   //no overlap
             return 0; 
 
         if(l <= start && end <= r)  //complete overlap
             return segment[idx];
 
+        //recursion
         int mid = start + (end - start) / 2;
 
         return query(2 * idx + 1, start, mid, l, r) + query(2 * idx + 2, mid + 1, end, l, r);
     }
 
+    //----------------------- update query O(log n) -------------------------------------------
+    void updateNode(int idx, int val){
+        int mid = (n - 1 )/ 2;
+        return updateQuery(idx, val, 0, 0, n-1);
+    }
+
+    void updateQuery(int idx, int val, int i, int l, int r){
+        //base case
+        if(l == r){
+            segment[i] = val;
+            return;
+        }
+
+        //recursion
+        int mid = l + (r - l) / 2;
+        if(idx <= mid){
+            
+            updateQuery(idx, val, 2 * i + 1, l, mid);
+        }
+        else{
+            updateQuery(idx, val, 2 * i + 2, mid + 1, r);
+        }
+
+        segment[i] = segment[2 * i + 1] + segment[2 * i + 2];
+    }
 };
 
 int main(){
@@ -64,10 +89,24 @@ int main(){
     Solution sol;
 
     sol.segmentTreeFormation(nums);
-    
-    for(int res : sol.segment){
+
+    //---------------------------------------
+    cout << "segment tree: ";
+    for (int res : sol.segment)
+    {
         cout << res << " , ";
     }
+    cout << endl;
+
+    //---------------------------------------
+    cout << "queries : sum between l = 2 to r = 5 -> " ;
+    cout << sol.getNodeValue(2, 5)<<endl;
+
+    //---------------------------------------
+    cout << "update idx = 2 with value = 1" << endl;
+    sol.updateNode(2, 1);
+
+    //---------------------------------------
 
     return 0;
 }
